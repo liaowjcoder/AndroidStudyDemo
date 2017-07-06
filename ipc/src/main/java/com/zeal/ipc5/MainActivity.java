0a73b4ecd2a7d54b36c272f55dfd9f4dbbbd2afd
zeal <lwj_zeal@163.com>
2017-07-07 00:07:59 +0800

    添加 book 接口的调用

--- ipc/src/main/java/com/zeal/ipc5/MainActivity.java
+++ ipc/src/main/java/com/zeal/ipc5/MainActivity.java
@@ -10,6 +10,9 @@ import android.os.RemoteException;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+
+import com.zeal.ipc.R;
 
 /**
  * Created by liaowj on 2017/7/6.
@@ -23,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        setContentView(R.layout.main);
+
         Intent service = new Intent(this, BookManagerService.class);
         bindService(service, conn, Service.BIND_AUTO_CREATE);
     }
@@ -30,10 +35,8 @@ public class MainActivity extends AppCompatActivity {
     private ServiceConnection conn = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName name, IBinder service) {
-
             //将服务端传递过来的 IBinder 转化为实际的对象 IBookManager
             mBookManager = IBookManager.Stub.asInterface(service);
-
             try {
                 //这里可能会造成线程阻塞
                 Log.e("zeal", mBookManager.getBookList().toString());
@@ -60,5 +63,24 @@ public class MainActivity extends AppCompatActivity {
         if (conn != null) {
             unbindService(conn);
         }
+    }
+
+    public void addBook(View view) {
+
+        Book book = new Book(3, "Android开发进阶");
+
+        if (mBookManager.asBinder().isBinderAlive()) {
+            try {
+                //调用远程服务方法，添加一本书
+                mBookManager.addBook(book);
+
+                //打印图书列表
+                Log.e("zeal", "AFTER ADD BOOK:"+mBookManager.getBookList().toString());
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
+
+
     }
 }

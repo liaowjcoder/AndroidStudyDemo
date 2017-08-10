00ad8cb173bf5145a1a26386dacccae0c4dde930
zeal <lwj_zeal@163.com>
2017-08-10 23:59:57 +0800

    杀死子进程的测试

--- ipc/src/main/java/com/zeal/ipc/Main2Activity.java
+++ ipc/src/main/java/com/zeal/ipc/Main2Activity.java
@@ -2,6 +2,7 @@ package com.zeal.ipc;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Process;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
@@ -17,6 +18,16 @@ public class Main2Activity extends AppCompatActivity {
         textview = (TextView) findViewById(R.id.textview);
 
         textview.setText("Main2Activity");
+
+
+        findViewById(R.id.kill).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //杀死自己
+                Process.killProcess(Process.myPid());
+                //System.exit(0);
+            }
+        });
     }
     public void onclick(View view) {
 
--- ipc/src/main/java/com/zeal/ipc/MainActivity.java
+++ ipc/src/main/java/com/zeal/ipc/MainActivity.java
@@ -1,10 +1,16 @@
 package com.zeal.ipc;
 
+import android.app.ActivityManager;
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Process;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+
+import java.util.List;
 
 /*
 一个应用开启多个进程，当某一个进程挂了，不会影响其他进程。
@@ -22,6 +28,24 @@ public class MainActivity extends AppCompatActivity {
         textview= (TextView) findViewById(R.id.textview);
 
         textview.setText("MainActivity");
+
+
+        findViewById(R.id.kill).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ActivityManager am = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+
+                List<ActivityManager.RunningAppProcessInfo> runningAppProcesses = am.getRunningAppProcesses();
+                for (ActivityManager.RunningAppProcessInfo runningAppProcess : runningAppProcesses) {
+                    String processName = runningAppProcess.processName;
+                    if("com.zeal.ipc:remote".equals(processName)) {
+                        Log.e("zeal","找到杀死子进程="+processName);
+                        Process.killProcess(runningAppProcess.pid);
+                        return;
+                    }
+                }
+            }
+        });
     }
 
     public void onclick(View view) {

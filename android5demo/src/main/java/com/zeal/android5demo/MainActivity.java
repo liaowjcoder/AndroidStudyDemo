21f7ebfed7effeacad6cc796de471f8e216b5373
zeal <lwj_zeal@163.com>
2017-08-29 19:37:59 +0800

    RecyclerView切换不同的布局管理器

---
+++ android5demo/src/main/java/com/zeal/android5demo/MainActivity.java
@@ -0,0 +1,131 @@
+package com.zeal.android5demo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import com.zhy.adapter.recyclerview.MultiItemTypeAdapter;
+import com.zhy.adapter.recyclerview.base.ItemViewDelegate;
+import com.zhy.adapter.recyclerview.base.ViewHolder;
+
+import java.util.ArrayList;
+import java.util.List;
+/*
+点击按钮切换不同的布局：GridLayoutManager 和 LinearLayoutManager之间切换
+ */
+public class MainActivity extends AppCompatActivity {
+
+    private List<String> datas = new ArrayList<>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        final RecyclerView recyclerview = (RecyclerView) findViewById(R.id.recyclerview);
+
+
+
+
+        MultiItemTypeAdapter<String> multiItemTypeAdapter = new MultiItemTypeAdapter<>(this,datas);
+
+        multiItemTypeAdapter.addItemViewDelegate(new AViewItemType());
+        multiItemTypeAdapter.addItemViewDelegate(new HeaderViewItemType());
+
+
+        final LinearLayoutManager lin= new LinearLayoutManager(this);
+        recyclerview.setLayoutManager(lin);
+
+        final GridLayoutManager grid = new GridLayoutManager(this,2);
+
+        grid.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+            @Override
+            public int getSpanSize(int position) {
+                if(position==0) {
+                    return grid.getSpanCount();
+                }
+                return 1;
+            }
+        });
+
+
+        initDatas();
+
+        recyclerview.setAdapter(multiItemTypeAdapter);
+
+
+        findViewById(R.id.switch_type).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(recyclerview.getLayoutManager() instanceof GridLayoutManager){
+                    recyclerview.setLayoutManager(lin);
+                }else{
+                    recyclerview.setLayoutManager(grid);
+                }
+            }
+        });
+
+
+
+
+
+
+    }
+
+    private void initDatas() {
+
+        datas.add("B");
+        datas.add("A1");
+        datas.add("A2");
+        datas.add("A3");
+        datas.add("A4");
+        datas.add("A5");
+        datas.add("A6");
+        datas.add("A7");
+        datas.add("A8");
+        datas.add("A9");
+        datas.add("A10");
+        datas.add("A11");
+        datas.add("A12");
+    }
+}
+
+
+class AViewItemType implements ItemViewDelegate<String>{
+
+    @Override
+    public int getItemViewLayoutId() {
+        return R.layout.normal;
+    }
+
+    @Override
+    public boolean isForViewType(String item, int position) {
+        return item.startsWith("A");
+    }
+
+    @Override
+    public void convert(ViewHolder holder, String s, int position) {
+        holder.setText(R.id.text,s);
+    }
+}
+
+class HeaderViewItemType implements ItemViewDelegate<String>{
+
+    @Override
+    public int getItemViewLayoutId() {
+        return R.layout.header;
+    }
+
+    @Override
+    public boolean isForViewType(String item, int position) {
+        return item.startsWith("B");
+    }
+
+    @Override
+    public void convert(ViewHolder holder, String s, int position) {
+    }
+}
